4c806be50a2c156c4c09aa2929dd1660262d7f6b
VoxWave <victor_bankowski@hotmail.com>
2018-04-10 20:40:33 +0300

    started with defining the enums related to tokens

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -8,6 +8,50 @@
 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-enum Token {
-    
+use num_bigint::BigInt;
+
+struct Token {
+    token_type: TokenType,
+    line: usize,
+    column: (usize, usize),
+}
+
+enum TokenType {
+    Identifier(String),
+    Literal(Literal),
+    Operator(Operator),
+}
+
+enum Literal {
+    Integer(i64),
+    Real(f64),
+    String(String),
+}
+
+enum Operator {
+    Plus,
+    Minus,
+    Mul,
+    Equals,
+}
+
+enum KeyWord {
+    Or,
+    And,
+    Not,
+    If,
+    Then,
+    Else,
+    Of,
+    While,
+    Do,
+    Begin,
+    End,
+    Var,
+    Array,
+    Procedure,
+    Function,
+    Program,
+    Assert,
+    Return,
 }
